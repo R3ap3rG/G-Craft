9d410f1758055ae7ee2ee03162c3d8c27231957b
R3ap3rG <owen_fielder@hotmail.com>
2015-01-10 19:30:57 +0000

    Initial Foundations
    Creation of mod class and various textures for future blocks/items

---
+++ src/main/java/com/R3ap3rG/gcraft/GCraft.java
@@ -0,0 +1,29 @@
+package com.R3ap3rG.gcraft;
+
+import com.R3ap3rG.gcraft.reference.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class GCraft {
+
+    @Mod.Instance
+    public static GCraft instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+}
---
+++ src/main/java/com/R3ap3rG/gcraft/reference/Reference.java
@@ -0,0 +1,10 @@
+package com.R3ap3rG.gcraft.reference;
+
+public class Reference {
+
+    public static final String MOD_ID = "GCraft";
+    public static final String VERSION = "Pre-Alpha_1.0";
+    public static final String MOD_NAME = "G-Craft";
+
+
+}
